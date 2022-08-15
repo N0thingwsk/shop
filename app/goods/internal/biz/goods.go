e72da45870e1b94e8030513a376c32c7bb78b361
playmaker <294270390@qq.com>
2022-08-15 15:48:02 +0800

    v1 添加商品服务api

--- app/goods/internal/biz/goods.go
+++ app/goods/internal/biz/goods.go
@@ -27,7 +27,7 @@ func (g *GormList) Scan(value interface{}) error {
 type Category struct {
 	gorm.Model
 	Name             string `gorm:"type:varchar(20);not null"`
-	ParentCategoryID int32
+	ParentCategoryID uint
 	ParentCategory   *Category
 	Level            int32 `gorm:"type:int;not null;default:1"`
 	IsTab            bool  `gorm:"default:false;not null"`
@@ -41,10 +41,14 @@ type Brands struct {
 
 type GoodsCategoryBrand struct {
 	gorm.Model
-	CategoryID int32 `gorm:"type:int;index:idx_category_brand,unique"`
+	CategoryID uint `gorm:"type:int;index:idx_category_brand,unique"`
 	Category   Category
-	BrandsID   int32 `gorm:"type:int;index:idx_category_brand,unique"`
+	BrandsID   uint `gorm:"type:int;index:idx_category_brand,unique"`
 	Brands     Brands
+}
+
+func (g GoodsCategoryBrand) TableName() string {
+	return "goodscategorybrand"
 }
 
 type Banner struct {
@@ -56,14 +60,14 @@ type Banner struct {
 
 type Goods struct {
 	gorm.Model
-	CategoryID      int32 `gorm:"type:int;index:idx_category_brand,unique"`
+	CategoryID      uint `gorm:"type:int;not null"`
 	Category        Category
-	BrandsID        int32 `gorm:"type:int;index:idx_category_brand,unique"`
+	BrandsID        uint `gorm:"type:int;not null"`
 	Brands          Brands
 	OnSale          bool     `gorm:"default:false;not null"`
-	ShipFree        bool     `gorm:"default;false;not null"`
-	IsNew           bool     `gorm:"default;false;not null"`
-	IsHot           bool     `gorm:"default;false;not null"`
+	ShipFree        bool     `gorm:"default:false;not null"`
+	IsNew           bool     `gorm:"default:false;not null"`
+	IsHot           bool     `gorm:"default:false;not null"`
 	Name            string   `gorm:"type:varchar(50);not null"`
 	GoodsSn         string   `gorm:"type:varchar(50);not null"`
 	ChickNum        int32    `gorm:"type:int;default:0;not null"`
@@ -71,7 +75,7 @@ type Goods struct {
 	FavNum          int32    `gorm:"type:int;default:0;not null"`
 	MarketPrice     float32  `gorm:"not null"`
 	ShopPrice       float32  `gorm:"not null"`
-	GoodsBrief      string   `gorm:"type:varchar(50);not null"`
+	GoodsBrief      string   `gorm:"type:varchar(100);not null"`
 	Images          GormList `gorm:"type:varchar(1000);not null"`
 	DescImages      GormList `gorm:"type:varchar(1000);not null"`
 	GoodsFrontImage string   `gorm:"type:varchar(200);not null"`
---
+++ app/goods/internal/test/gorm_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"shop/app/goods/internal/biz"
+	"testing"
+)
+
+func TestGorm(t *testing.T) {
+	db, err := gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:3306)/shop_goods?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+	err = db.AutoMigrate(&biz.Category{}, biz.Brands{}, &biz.GoodsCategoryBrand{}, &biz.Banner{}, &biz.Goods{})
+	if err != nil {
+		panic(err.Error())
+	}
+}
